ededffeda59def6da2587e9c8877e97e43126140
ImmortalIdiot <max.krasulya@yandex.ru>
2025-05-14 11:58:39 +0300

    refactor: small changes

--- src/main/kotlin/adapter/secondary/persistance/ProductRepository.kt
+++ src/main/kotlin/adapter/secondary/persistance/ProductRepository.kt
@@ -8,13 +8,13 @@ class ProductRepository : ProductRepository {
     private val products = HashMap<String, Product>()
 
     init {
-        addProduct(Product("1", "Творог", 120.0, LocalDate.now().plusDays(5)))
-        addProduct(Product("2", "Курица", 300.0, LocalDate.now().minusDays(13)))
+        addProduct(Product("1", "Творог", 200.0, LocalDate.now().plusDays(5)))
+        addProduct(Product("2", "Курица", 500.0, LocalDate.now().minusDays(13)))
         addProduct(Product("3", "Молоко", 90.0, LocalDate.now().plusDays(7)))
-        addProduct(Product("4", "Яйца", 85.0, LocalDate.now().plusDays(10)))
-        addProduct(Product("5", "Картофель", 50.0, LocalDate.now().plusDays(20)))
+        addProduct(Product("4", "Яйца", 120.0, LocalDate.now().plusDays(10)))
+        addProduct(Product("5", "Лимонад", 50.0, LocalDate.now().plusDays(20)))
         addProduct(Product("6", "Пельмени", 250.0, LocalDate.now().plusDays(15)))
-        addProduct(Product("7", "Селёдка", 600.0, LocalDate.now().minusDays(5)))
+        addProduct(Product("7", "Селёдка", 400.0, LocalDate.now().minusDays(5)))
     }
 
     private fun addProduct(product: Product) {
